058a189727fc3667c386798dec0d1aa828cbe239
esrafilelahi <essinho1121@gmail.com>
2023-07-29 13:09:41 +0330

    add counter

---
+++ Counter.js
@@ -0,0 +1,19 @@
+// File: Counter.js
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={handleIncrement}>Increment</button>
+    </div>
+  );
+};
+
+export default Counter;
