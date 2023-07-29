e4ecb1eb06021f379bf2e3963ecb3b65a9dd93ba
esrafilelahi <essinho1121@gmail.com>
2023-07-29 13:10:47 +0330

    add todolist file

---
+++ TodoList.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleInputChange = (e) => {
+    setNewTodo(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, newTodo.trim()]);
+      setNewTodo('');
+    }
+  };
+
+  const handleDeleteTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo} <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <input type="text" value={newTodo} onChange={handleInputChange} />
+      <button onClick={handleAddTodo}>Add Todo</button>
+    </div>
+  );
+};
+
+export default TodoList;
