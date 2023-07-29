d35393ffe605b329f4d7385c112fb8afd0f4aa1b
Esrafil <essinho1121@gmail.com>
2023-07-29 17:05:49 +0330

    Update Counter.test.js

--- unit/Counter.test.js
+++ unit/Counter.test.js
@@ -3,7 +3,7 @@ import { render, fireEvent } from '@testing-library/react';
 import Counter from './Counter';
 
 test('handleIncrement function increments count correctly', () => {
-  // Render the component
+  // rendering the component we want to test
   const { getByText } = render(<Counter />);
 
   // Get the button element
