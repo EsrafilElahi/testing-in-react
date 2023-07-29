ae83a1cbc8f28f23a8f051a7cdd4f481b9c57d09
esrafilelahi <essinho1121@gmail.com>
2023-07-29 13:13:41 +0330

    add login form and welcom mesage file

---
+++ LoginForm.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+const LoginForm = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Simulate login logic (e.g., API call)
+    // For simplicity, we'll assume login is successful after a short delay
+    setTimeout(() => {
+      onLogin(username);
+    }, 500);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ WelcomeMessage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WelcomeMessage = ({ username }) => {
+  return <div>Welcome, {username}!</div>;
+};
+
+export default WelcomeMessage;
